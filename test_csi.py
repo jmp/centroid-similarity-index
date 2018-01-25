49a1813f4fad393ab86537fa52861d2467f55ef5
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2018-01-25 19:58:40 +0200

    Move tests.

--- test_csi.py
+++ test_csi.py
@@ -1,17 +1,16 @@
-from unittest import TestCase
+import unittest
+import csi
 
-from csi import csi, find_nearest, calculate_mapping, calculate_num_shared_points, calculate_num_points_in_solution
 
-
-class FindNearestTest(TestCase):
+class FindNearestTest(unittest.TestCase):
     def test_find_nearest(self):
-        self.assertEqual((1, 1), find_nearest((0, 0), [(1, 1), (1, 2), (1, 3)]))
-        self.assertEqual((1, 1), find_nearest((0, 0), [(1, 2), (1, 1), (1, 3)]))
+        self.assertEqual((1, 1), csi.find_nearest((0, 0), [(1, 1), (1, 2), (1, 3)]))
+        self.assertEqual((1, 1), csi.find_nearest((0, 0), [(1, 2), (1, 1), (1, 3)]))
 
 
-class CalculateMappingTest(TestCase):
+class CalculateMappingTest(unittest.TestCase):
     def test_calculate_mapping_trivial(self):
-        mapping = calculate_mapping([(0, 0)], [(1, 1)])
+        mapping = csi.calculate_mapping([(0, 0)], [(1, 1)])
         self.assertDictEqual(mapping, {
             (0, 0): (1, 1),
         })
@@ -19,8 +18,8 @@ class CalculateMappingTest(TestCase):
     def test_calculate_mapping_symmetric(self):
         cluster1 = [(0, 0), (0, 1)]
         cluster2 = [(-1, 0), (0, 2)]
-        mapping1 = calculate_mapping(cluster1, cluster2)
-        mapping2 = calculate_mapping(cluster2, cluster1)
+        mapping1 = csi.calculate_mapping(cluster1, cluster2)
+        mapping2 = csi.calculate_mapping(cluster2, cluster1)
         self.assertDictEqual(mapping1, {
             (0, 0): (-1, 0),
             (0, 1): (0, 2),
@@ -33,8 +32,8 @@ class CalculateMappingTest(TestCase):
     def test_calculate_mapping_asymmetric(self):
         cluster1 = [(0, 0), (0, 1)]
         cluster2 = [(-1, 0), (0, 2), (0, 3)]
-        mapping1 = calculate_mapping(cluster1, cluster2)
-        mapping2 = calculate_mapping(cluster2, cluster1)
+        mapping1 = csi.calculate_mapping(cluster1, cluster2)
+        mapping2 = csi.calculate_mapping(cluster2, cluster1)
         self.assertDictEqual(mapping1, {
             (0, 0): (-1, 0),
             (0, 1): (0, 2),
@@ -46,45 +45,45 @@ class CalculateMappingTest(TestCase):
         })
 
 
-class SolutionPointsTest(TestCase):
+class SolutionPointsTest(unittest.TestCase):
     def test_calculate_num_points_in_empty_solution(self):
-        self.assertEqual(0, calculate_num_points_in_solution({}))
+        self.assertEqual(0, csi.calculate_num_points_in_solution({}))
 
     def test_calculate_num_points_in_solution_single_cluster(self):
-        self.assertEqual(1, calculate_num_points_in_solution({(0, 0): [(0, 0)]}))
-        self.assertEqual(2, calculate_num_points_in_solution({(0, 0): [(0, 0), (1, 1)]}))
+        self.assertEqual(1, csi.calculate_num_points_in_solution({(0, 0): [(0, 0)]}))
+        self.assertEqual(2, csi.calculate_num_points_in_solution({(0, 0): [(0, 0), (1, 1)]}))
 
     def test_calculate_num_points_in_solution_multiple_clusters(self):
-        self.assertEqual(3, calculate_num_points_in_solution({
+        self.assertEqual(3, csi.calculate_num_points_in_solution({
             (0, 0): [(0, 0)],
             (1, 1): [(1, 1)],
             (2, 2): [(2, 2)],
         }))
-        self.assertEqual(6, calculate_num_points_in_solution({
+        self.assertEqual(6, csi.calculate_num_points_in_solution({
             (0, 0): [(0, 0)],
             (1, 1): [(1, 1), (4, 4)],
             (2, 2): [(3, 3), (5, 5), (7, 7)],
         }))
 
 
-class SharedPointsTest(TestCase):
+class SharedPointsTest(unittest.TestCase):
     def test_calculate_num_shared_points_none(self):
         cluster1 = [(0, 0), (1, 1)]
         cluster2 = [(2, 2), (3, 3)]
-        self.assertEqual(0, calculate_num_shared_points(cluster1, cluster2))
+        self.assertEqual(0, csi.calculate_num_shared_points(cluster1, cluster2))
 
     def test_calculate_num_shared_points_some(self):
         cluster1 = [(0, 0), (1, 1)]
         cluster2 = [(2, 2), (0, 0)]
-        self.assertEqual(1, calculate_num_shared_points(cluster1, cluster2))
+        self.assertEqual(1, csi.calculate_num_shared_points(cluster1, cluster2))
 
     def test_calculate_num_shared_points_all(self):
         cluster1 = [(0, 0), (1, 1)]
         cluster2 = [(1, 1), (0, 0)]
-        self.assertEqual(2, calculate_num_shared_points(cluster1, cluster2))
+        self.assertEqual(2, csi.calculate_num_shared_points(cluster1, cluster2))
 
 
-class CsiTest(TestCase):
+class CsiTest(unittest.TestCase):
     def test_csi_same(self):
         cluster1 = [(5, 0), (0, 5)]
         cluster2 = [(6, 0), (0, 6)]
@@ -96,7 +95,8 @@ class CsiTest(TestCase):
             (2, 2): cluster1,
             (3, 3): cluster2,
         }
-        self.assertEqual(1, csi(solution1, solution2))
+        self.assertEqual(1, csi.csi(solution1, solution2))
+        self.assertEqual(1, csi.csi(solution2, solution1))
 
     def test_csi_some_different(self):
         cluster1 = [(5, 0), (4, 1), (3, 2), (2, 2), (5, 1), (5, 3)]
@@ -109,7 +109,8 @@ class CsiTest(TestCase):
             (2, 2): cluster1,
             (3, 1): cluster2,
         }
-        self.assertEqual(0.75, csi(solution1, solution2))
+        self.assertEqual(0.75, csi.csi(solution1, solution2))
+        self.assertEqual(0.75, csi.csi(solution2, solution1))
 
     def test_csi_all_different(self):
         cluster1 = [(5, 0), (4, 1), (3, 2), (2, 2), (5, 1), (5, 3)]
@@ -122,5 +123,35 @@ class CsiTest(TestCase):
             (13, 1): cluster1,
             (2, 2): cluster2,
         }
-        self.assertEqual(0, csi(solution1, solution2))
+        self.assertEqual(0, csi.csi(solution1, solution2))
+        self.assertEqual(0, csi.csi(solution2, solution1))
 
+    def test_csi_different_number_of_clusters(self):
+        cluster1 = [(5, 0), (0, 5)]
+        cluster2 = [(6, 0), (0, 6)]
+        cluster3 = [(7, 0), (0, 7)]
+        solution1 = {
+            (2, 2): cluster1,
+            (3, 3): cluster2,
+            (4, 4): cluster3,
+        }
+        solution2 = {
+            (2, 2): cluster1,
+            (3, 3): cluster2,
+        }
+        self.assertEqual(0.8, csi.csi(solution1, solution2))
+        self.assertEqual(0.8, csi.csi(solution2, solution1))
+
+    def test_csi_points_missing(self):
+        cluster1 = [(5, 0), (0, 5), (0, 4), (0, 1)]
+        cluster2 = [(6, 0), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10)]
+        solution1 = {
+            (2, 2): cluster1,
+            (3, 3): cluster2,
+        }
+        solution2 = {
+            (4, 5): [],
+            (6, 7): cluster2,
+        }
+        self.assertEqual(0.375, csi.csi(solution1, solution2))
+        self.assertEqual(0.375, csi.csi(solution2, solution1))
